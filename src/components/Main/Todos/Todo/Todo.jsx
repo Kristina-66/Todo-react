de5dad4ee86fb17fb3b838940e5b10aaad0a0253
Kristina <dead.girl.2012@mail.ru>
2022-05-25 17:35:51 +0300

    add fwature1 work

---
+++ src/components/Main/Todos/Todo/Todo.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styles from "./todo.module.css";
+export default function Todo({ todo, todos, setTodos, tabs }) {
+  
+    const handleClick = (id) => {
+      const newArray = todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      setTodos(newArray);
+    };
+   
+    const handleDelete = (id) => {
+      const newArray = todos.filter((todo) => todo.id !== id);
+      setTodos(newArray);
+    };
+    
+    return (
+      <div className={styles.wrapperTodoandDelete}>
+        <div
+          key={todo.id}
+          className={styles.wrapperTodo}
+          onClick={() => {
+            handleClick(todo.id);
+          }}
+        >
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={todo.completed}
+          />{" "}
+          <span
+            className={
+              todo.completed
+                ? `${styles.textTodo} ${styles.stroke}`
+                : styles.textTodo
+            }
+          >
+            {" "}
+            {todo.text}
+          </span>
+        </div>
+        {tabs[2].isActive && (
+         <svg
+         className={styles.path}
+         xmlns="http://www.w3.org/2000/svg"
+         
+         height="48px"
+         width="48px"
+         onClick={() => handleDelete(todo.id)}
+       >
+         <path d="M15 39H33Q33 39 33 39Q33 39 33 39V15H15V39Q15 39 15 39Q15 39 15 39ZM10.5 11V8H17.2L19.2 6H28.8L30.8 8H37.5V11ZM15 42Q13.8 42 12.9 41.1Q12 40.2 12 39V12H36V39Q36 40.2 35.1 41.1Q34.2 42 33 42ZM15 39H33Q33 39 33 39Q33 39 33 39H15Q15 39 15 39Q15 39 15 39Z" />
+       </svg>
+        )} 
+      </div>
+    );
+  }
--- src/components/Main/Todos/Todos.jsx
+++ src/components/Main/Todos/Todos.jsx
@@ -1,20 +1,43 @@
 import React from "react";
+import Todo from "./Todo/Todo";
 import styles from "./todos.module.css";
 
-export default function Todos({ todos }) {
-  return (
-    <div>
-      {todos.map((todo) => (
-        <div className={styles.inputbox1}>
-        <p>
-           <input className={styles.inputbox} type="checkbox" />
-          {todo.text} была создана {todo.date.toLocaleString()}
-        </p>
-</div>
-
-      ))}
-      
+export default function Todos({ todos, setTodos, tabs }) {
   
-    </div>
+  const deleteAllClickHandler = () => {
+    const newArray = todos.filter((todo) => todo.completed === false);
+    setTodos(newArray);
+  };
+  
+  return (
+    <>
+      {todos.map((todo) => {
+        if (tabs[0].isActive) {
+          return (
+            <Todo todos={todos} setTodos={setTodos} todo={todo} tabs={tabs} />
+          );
+        } else if (tabs[1].isActive && !todo.completed) {
+          return (
+            <Todo todos={todos} setTodos={setTodos} todo={todo} tabs={tabs} />
+          );
+        } else if (tabs[2].isActive) {
+          return todo.completed ? (
+            <Todo todos={todos} setTodos={setTodos} todo={todo} tabs={tabs} />
+          ) : null;
+        }
+        return null;
+      })}
+      <div className={styles.btnDeleteAllWrapper}>
+        {tabs[2].isActive && (
+          <button
+            type="button"
+            className={styles.deleteAll}
+            onClick={() => deleteAllClickHandler()}
+          >
+            delete all
+          </button>
+        )}
+      </div>
+    </>
   );
 }
